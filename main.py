38963e17cbca63118be44020fd8c7f036fd537a6
jorygut <118095034+jorygut@users.noreply.github.com>
2024-06-11 11:15:41 -0700

    m

--- main.py
+++ main.py
@@ -476,8 +476,6 @@ def analyze_patterns(df):
 def upload_image_and_number():
     print('image uploaded')
 
-    if 'xml_file' not in request.files:
-        return jsonify({"error": "No XML file part in the request"}), 400
     #Get data from API
     image_file = request.files['image_file']
     number = request.form['number']
